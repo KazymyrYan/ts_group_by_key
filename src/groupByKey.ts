789a3685f4aaa19b7ac1b4d5d22cadaaf6bb9eb4
Yan Kazymyr <kazymyr.yan@gmail.com>
2025-10-04 18:41:46 +0300

    Fixed v.2

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,12 +2,20 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+export function groupByKey<T, K extends keyof T>(
+  items: T[],
+  key: K,
+): GroupsMap<T> {
   const accum: GroupsMap<T> = {};
 
   for (const item of items) {
     const rawValue = item[key];
-    const value = rawValue !== null ? String(rawValue) : 'undefined';
+    const value =
+      rawValue === null
+        ? 'null'
+        : rawValue === undefined
+          ? 'undefined'
+          : String(rawValue);
 
     if (!accum[value]) {
       accum[value] = [];
